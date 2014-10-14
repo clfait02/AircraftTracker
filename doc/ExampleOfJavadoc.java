d84141885639f5d23531239e2ce0dce7e37da784
Ulf Schmelzer <ulf.schmelzer@stud.hs-esslingen.de>
2014-10-14 16:19:55 +0200

    file with example who to use javadoc added

---
+++ doc/ExampleOfJavadoc.java
@@ -0,0 +1,17 @@
+/**
+ * Ein Hello-World-Programm in Java.
+ * Dies ist ein Javadoc-Kommentar.
+ *
+ * @author John Doe
+ * @version 1.0
+ */
+public class Hello {
+    /** 
+     * Hauptprogramm.
+     *
+     * @param args Kommandozeilenparameter
+     */
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt!");
+    }
+}
