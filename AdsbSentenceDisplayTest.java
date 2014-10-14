c4805f175c90211f6e4913db74dc61644a1ea826
Ulf Schmelzer <ulf.schmelzer@stud.hs-esslingen.de>
2014-10-14 16:49:00 +0200

    first test added to test junit

---
+++ AdsbSentenceDisplayTest.java
@@ -0,0 +1,22 @@
+/**
+ * Junit test for display class
+ * Test that the object is created correctly and out is in the correct format
+ *
+ * @author John Doe
+ * @version 1.0
+ */
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class AdsbSentenceDisplayTest{
+    /** 
+     * Test the junit
+     */
+    @Test
+    public outPutFirstTest {
+        assertEquals("this test have to pass" , 0 , 0);
+    }
+}
+
+
