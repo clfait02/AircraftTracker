46ede2d0cfb640e6fd2ab12fabb8b43ba6fb30cc
Ulf Schmelzer <ulf.schmelzer@stud.hs-esslingen.de>
2014-10-14 16:08:35 +0200

    class body implemented

---
+++ AdsbSentenceDisplay.java
@@ -0,0 +1,19 @@
+/**
+ * This class is to print adsb messages in the following format to stdout 
+ * Time:          Weekday, DD.MM.YYYY, hrs:min:sec.usec
+ * Dfca:          8D
+ * Originator:    4692CA
+ * Payload:       584720707A0996
+ * Parity:        49890A
+ * 
+ * Author: Ulf Schmelzer ulscit01@hs-esslingen.de
+ * Date: 14.10.2014
+ */
+class AdsbSentenceDisplay{
+   public void display(AdsbSentence sentence){
+        
+
+   }
+
+   private 
+}
